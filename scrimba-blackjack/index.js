11152f7da41663ca121694cc3ab78d40078e5a92
oni <vex@aol.jp>
2022-02-25 11:16:02 +0100

    Add more future in JS
    array,
    objects,
    booleans,
    if else statements,
    comparison operators,
    logical operators (and, or xor),
    for loop,
    the Math object,
    return statements

--- scrimba-blackjack/index.js
+++ scrimba-blackjack/index.js
@@ -1,21 +1,55 @@
-let firstCard = 10
-let secondCard = 10
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let player = {
+    name:"Klopp",
+    chips:145
+    /*sayHallo: function(){
+        console.log("Hi!!")
+    }*/
+}
 
+let cards = [] // ordered list of items
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
+let playerEl = document.getElementById('player-el')
+
+// Render the players name and chips in plyerEl
+playerEl.textContent = player.name + ": €" + player.chips
+
+function getRandomCard(){
+    // 1-13
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+
+    // if 1 -> return 11
+    // if 11-13 -> return 10 
+    if (randomNumber > 10){
+        return 10
+    }else if(randomNumber === 1){
+        return 11
+    }else{
+        return randomNumber
+    }
+}
 
 function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
-
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    // render out firstCard and secondCard
+    cardsEl.textContent = "Cards: " //+ cards[0] + " " + cards[1]
+    for(let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    // render out all the cards we have 
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -30,8 +64,12 @@ function renderGame() {
 }
 
 function newCard(){
-    console.log('Drawing a new Card from the deck!')
-    let card = 7
-    sum += card
-    renderGame()
+    if (isAlive === true && hasBlackJack === false){
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        //console.log(cards)
+        renderGame()
+    }
+
 }
